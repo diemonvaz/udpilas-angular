e5788e5a7d5f25787a153fdd6f7bb024e84ff974
Iván Martín Jiménez <79899723+ivanmartinsk@users.noreply.github.com>
2022-11-06 20:24:09 +0100

    cambios BD

--- API/src/entity/Usuarios.ts
+++ API/src/entity/Usuarios.ts
@@ -9,17 +9,22 @@ export class Usuarios {
     idusuario: number;
 
     @Column({type: "varchar", width: 255, default: null, nullable: true})
-    nombre_completo: string;
+    nombre: string;
 
     @Column({type: "varchar", width: 255, default: null, nullable: true})
-    correo: string;
+    apellidos: string;
 
-    @Column({type: "text", nullable: false})
-    fecha_nacimiento: string;
+    @Column({type: "varchar", width: 255, default: null, nullable: true})
+    email: string;
+
+    @Column({type: "date", nullable: false})
+    fecha_nacimiento: Date;
 
     @Column({type: "varchar", width: 255, default: null, nullable: true})
     password: string;
 
+    @Column({type: "varchar", width: 255, default: null, nullable: true})
+    telefono: string;
 
     checkPassword(password: string):boolean{
         return bcrypt.compareSync(password, this.password);
